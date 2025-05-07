8ab670791bfb14c5a69a17ceb1470d7bf5becbe2
Aidan Cioppa <149898531+AidanCioppa04@users.noreply.github.com>
2025-05-07 12:58:59 +0900

    5/7/25

--- Rateable.java
+++ Rateable.java
@@ -1,3 +1,22 @@
+import java.util.List;
+
 public class Rateable {
-    //
+    String name;
+    //image???//
+    String tags[];
+
+    public Rateable(String name){
+        this.name=name;
+    }
+
+    //adds a tag to the objects tag list//
+    //0 - error
+    //1 - tag added successfully
+    //2 - object already had the tag
+    //3 - 
+    public static int addTag(String tag){
+
+
+        return 0;
+    }
 }
