811285092518850d29e89ec206a291450f022380
Aidan Cioppa <149898531+AidanCioppa04@users.noreply.github.com>
2025-05-08 14:31:34 +0900

    5/8

--- Rateable.java
+++ Rateable.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
 public class Rateable {
-    final String name;
+    String name;
     //image???//
-    static HashSet<String> tags;
+    HashSet<String> tags;
 
     public Rateable(String name){
-        this.name=name;
-        tags=new HashSet<String>;
+        setName(name);
+        tags=new HashSet<String>();
     }
 
     //adds a tag to the objects tag list//
@@ -16,10 +17,25 @@ public class Rateable {
     //1 - tag added successfully
     //2 - object already had the tag
     //3 - 
-    private static void addTag(String tag){
+    private void setTag(String tag){
+        tag=tag.toLowerCase();
         tags.add(tag);
+    }
+    //returns list of tags in alphabetical order//
+    public ArrayList<String> getTags(){
+        ArrayList<String> toSortTags=new ArrayList<>();
+        for(String tag:tags){
+            toSortTags.add(tag);
+        }
+        //might want to make tags class
+        //to make custom sorting
+        toSortTags.sort(null);
+        return toSortTags;
     }
     public String getName(){
         return name;
+    }
+    private void setName(String name){
+        this.name=name;
     }
 }
